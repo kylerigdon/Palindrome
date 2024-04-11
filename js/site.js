6f3677fc22c6fbbc83da4c7b8e86e99010cfe313
Kyle Rigdon <krigdon64@gmail.com>
2024-04-11 17:47:31 -0500

    Added a new logo, descriptions on the home page, app page and code page. Created javascript code to check if text entered is a palindrome

--- js/site.js
+++ js/site.js
@@ -1,13 +1,57 @@
+function getValues(){
 
-// Display the message the user entered.
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    let message = inputBox.value;
+  let inputString = document.getElementById('inputString').value;
+  inputLower = inputString.toLowerCase().replace(/[\W_]/g, '');
 
+  if (inputLower == 0) {
     Swal.fire({
-        title: "App Name",
-        text: message,
-        icon: "success",
-        backdrop: false
-      });
+      icon: "error",
+      title: "Oops!",
+      text: 'Please enter text.',
+      backdrop: false
+    });
+  } else {
+
+    let reversedString = [];
+
+  for (let i = inputLower.length - 1; i >= 0; i--){
+    reversedString += inputLower[i];
+  }
+    let checkedPalindrome = checkForPalindrome(inputLower, reversedString);
+
+    displayResults(checkedPalindrome, reversedString);
+  }
+}
+
+function checkForPalindrome(inputLower, reversedString){
+
+  let palindrome = ''
+
+  if (reversedString == inputLower){
+    palindrome = 'true'
+  } else {
+    palindrome = 'false'
+  }
+
+  return palindrome;
+}
+
+function displayResults(checked, reversed){
+
+  if (checked == 'true'){
+    Swal.fire({
+      icon: "success",
+      title: "Well done! You entered a Palindrome!",
+      text: `Your text reversed is: ${reversed}`,
+      backdrop: false
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Sorry. You did not enter a Palindrome.",
+      text: `Your text reversed is: ${reversed}`,
+      backdrop: false
+     });
+  }
+
 }
